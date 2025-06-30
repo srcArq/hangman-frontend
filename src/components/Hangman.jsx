3fbf64d1c691047da8bc11473be01685cd02b09c
Arquelia Conejo Lora <arquelia@hotmail.es>
2025-06-30 14:32:11 +0200

    Modificacion de las url para web

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -8,7 +8,7 @@ const Hangman = () => {
     const [level, setLevel] = useState(null);
 
     async function selectLevel(level) {
-        const response = await fetch('http://localhost:8080/api/selectLevel', {
+        const response = await fetch('https://hangman-backend-o08j.onrender.com/api/selectLevel', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
