623d8078915a131808d4a28f860641f0294972bf
Arquelia Conejo Lora <arquelia@hotmail.es>
2025-07-09 16:04:41 +0200

    Se modifica palabra de prueba a produccion

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -39,7 +39,7 @@ const Hangman = () => {
             {word === null && <LevelSelector onSelectLevel={selectLevel} />}
             {attempts && maskedWord && <BoardGame attemptsOriginal={{ attempts }} maskedWordOriginal={{ maskedWord }} levelOriginal={{ level }} />}
             
-            <p>{word}</p>
+            {/* <p>{word}</p> */}
         </>
     );
 };
