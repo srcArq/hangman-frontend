257fec106cba8dc6d388c1c8cd857456fff8b460
Arquelia Conejo Lora <arquelia@hotmail.es>
2025-07-29 20:03:46 +0200

    Se añade barra de carga, para retroaccion al usuario

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -1,44 +1,54 @@
+import { useState } from "react";
 import BoardGame from "./BoardGame";
 import LevelSelector from "./LevelSelector";
-import { useState } from "react";
-
-
+import Loader from "./Loader";
 
 const Hangman = () => {
     const [word, setWord] = useState(null);
     const [attempts, setAttempts] = useState(null);
     const [maskedWord, setMaskedWord] = useState(null);
     const [level, setLevel] = useState(null);
+    const [loading, setLoading] = useState(false);
+
     // Import the public API URL from environment variables
     const PUBLIC_API_URL = import.meta.env.PUBLIC_API_URL;
 
     async function selectLevel(level) {
-        const response = await fetch(`${PUBLIC_API_URL}/api/selectLevel`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ level }),
-        });
+        setLoading(true);  // Show loader when is selecting level
+        try {
+            const response = await fetch(`${PUBLIC_API_URL}/api/selectLevel`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ level }),
+            });
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+
+            setWord(data.selectedWord);
+            setAttempts(data.attempts);
+            setMaskedWord(data.maskedWord);
+            setLevel(level);
+
+            console.log('Selected level:', data);
+        } catch (error) {
+            console.error('Error selecting level:', error);
+        } finally {
+            setLoading(false);  // Hide loader after level is selected
         }
-        const data = await response.json();
-
-        setWord(data.selectedWord);
-        setAttempts(data.attempts);
-        setMaskedWord(data.maskedWord);
-        setLevel(level);
-
-        console.log('Selected level:', data);
     }
     return (
         <>
-        	<h1 className={`title ${word ? 'second-step' : ''}`}>AHORCADO</h1>
+            <h1 className={`title ${word ? 'second-step' : ''}`}>AHORCADO</h1>
+            {/** While loading, show Spinner **/}
+            {loading && <Loader />}
             {word === null && <LevelSelector onSelectLevel={selectLevel} />}
             {attempts && maskedWord && <BoardGame attemptsOriginal={{ attempts }} maskedWordOriginal={{ maskedWord }} levelOriginal={{ level }} />}
-            
+
             {/* <p>{word}</p> */}
         </>
     );
---
+++ src/components/Loader.jsx
@@ -0,0 +1,18 @@
+// src/LoadingBar.js
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-bar">
+        {/* Generate 10 segments to simulate the bar */}
+        {Array.from({ length: 10 }).map((_, index) => (
+          <div key={index} className="loading-segment"></div>
+        ))}
+      </div>
+      <p className="loading-text">CARGANDO...</p>
+    </div>
+  );
+};
+
+export default Loader;
